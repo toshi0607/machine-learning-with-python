63c7ac3d87fc63a454c99d8e4e26bda052d0f7b1
toshi0607 <s.toshi0607@gmail.com>
2017-01-22 14:53:35 +0900

    add lesson3

---
+++ lesson3-ans.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import expit
+
+m = 30
+n = 1
+theta0, theta1 = -8, 1.6
+
+x0 = np.arange(0, 30, 0.1)
+y0 = expit(theta0 + theta1 * x0)
+
+x = np.ones((m, n+1))
+x[:, 1] = np.random.rand(m) * 10
+y = np.ceil(expit(theta0 + theta1 * x[:, 1]) - np.random.rand(m))
+
+plt.xlim(0, 12)
+plt.ylim(-0.1, 1.1)
+plt.title("Tumors")
+plt.xlabel("size (mm)")
+plt.ylabel("malignant?")
+
+plt.plot(x0, y0)
+plt.plot(x[:, 1], y, "ko");
